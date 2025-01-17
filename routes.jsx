7289f8fcda3d49a2b347b1d7745dc3602c2f3027
daviiisousa <sousadavi248@gmail.com>
2025-01-17 17:06:57 -0300

    base

---
+++ routes.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Home } from "./src/pages/home/home"
+import { Cartas } from "./src/pages/cartas/cartas"
+import { CartasUsuarios } from "./src/pages/cartasUsuario/cartasUsuario"
+
+export const RoutesApp =  () => {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/cartas" element={<Cartas />} />
+                <Route path="/cartas-usuario" element={<CartasUsuarios />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
---
+++ src/pages/cartas/cartas.jsx
@@ -0,0 +1,9 @@
+import { Header } from "../../components/layout/Header/header"
+
+export const Cartas = () => {
+    return(
+        <>
+            <Header />
+        </>
+    )
+}
